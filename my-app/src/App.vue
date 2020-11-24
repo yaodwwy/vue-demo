0dfece897cbf3c3a0a35fb50d6c6b218279b967f
Adam.Yao <yaodwwy@gmail.com>
2020-11-24 22:15:15 +0800

    init

---
+++ my-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    hi
+  </div>
+  <navibar></navibar>
+  <sidebar></sidebar>
+</template>
+
+<script>
+import navibar from '.components/Navibar'
+import sidebar from '.components/Sidebar'
+
+export default {name: 'App',
+  data(){
+    return {
+      dataList:[]
+    }
+  },
+  methods:{
+    handleAdd(){
+      this.dataList.push('$refs.')
+    }
+  },
+  components:{
+    navibar:navibar,
+    sidebar:sidebar
+  }
+}
+</script>
---
+++ my-app/src/components/Navibar.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  navi-bar
+</div>
+</template>
+
+<script>
+export default {
+  name: "Navibar"
+}
+</script>
+
+<style scoped>
+
+</style>
